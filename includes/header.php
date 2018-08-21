472e69bab05fd75d05952e7faced1046fb9e2c8b
Yikai Zhang <zhangyikai@hotmail.com>
2018-08-21 11:19:38 -0400

    creating the music player

--- includes/header.php
+++ includes/header.php
@@ -21,8 +21,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Lou</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 <body>
+
     <div id="mainContainer">
         <div id="topContainer">
             <?php include('includes/navBarContainer.php'); ?>
